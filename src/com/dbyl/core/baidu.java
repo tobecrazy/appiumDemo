8d8afe3154c48c9bc3f6556c59c619df721238d6
tobecrazy <dbyl@foxmail.com>
2015-09-24 23:59:10 +0800

    Signed-off-by: tobecrazy <dbyl@foxmail.com>

--- src/com/dbyl/core/baidu.java
+++ src/com/dbyl/core/baidu.java
@@ -1,14 +1,20 @@
 package com.dbyl.core;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -48,6 +54,45 @@ public class baidu {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		System.out.println(driver.getTitle());
+		snapshot((TakesScreenshot) driver, "before_search.png");
+		driver.findElementByXPath("//textarea[@id='index-kw']").sendKeys(
+				"appium");
+		driver.findElement(By.xpath("//button[@id='index-bn']")).click();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		System.out.println(driver.getTitle());
+		snapshot((TakesScreenshot) driver, "after_search.png");
+		Assert.assertTrue(driver.getTitle().contains("appium"));
+
+	}
+
+	/**
+	 * This Method create for take screenshot
+	 * 
+	 * @author Young
+	 * @param drivername
+	 * @param filename
+	 */
+	public static void snapshot(TakesScreenshot drivername, String filename) {
+		// this method will take screen shot ,require two parameters ,one is
+		// driver name, another is file name
+
+		String currentPath = System.getProperty("user.dir"); // get current work
+																// folder
+		File scrFile = drivername.getScreenshotAs(OutputType.FILE);
+		// Now you can do whatever you need to do with it, for example copy
+		// somewhere
+		try {
+			System.out.println("save snapshot path is:" + currentPath + "/"
+					+ filename);
+			FileUtils
+					.copyFile(scrFile, new File(currentPath + "\\" + filename));
+		} catch (IOException e) {
+			System.out.println("Can't save screenshot");
+			e.printStackTrace();
+		} finally {
+			System.out.println("screen shot finished, it's in " + currentPath
+					+ " folder");
+		}
 	}
 
 	@AfterClass
