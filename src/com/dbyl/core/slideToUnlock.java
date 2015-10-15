66785eaf01507a1d8c436a08de9028a83b20990f
tobecrazy <dbyl@foxmail.com>
2015-10-15 23:49:23 +0800

    add swipe to unlock

--- src/com/dbyl/core/slideToUnlock.java
+++ src/com/dbyl/core/slideToUnlock.java
@@ -1,39 +1,69 @@
 package com.dbyl.core;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.MobileDriver;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class slideToUnlock {
-	private AndroidDriver<WebElement> driver;
+	private MobileDriver driver;
 
 	/**
+	 * 
 	 * @author Young
 	 * @throws IOException
 	 */
-	public void startRecord() throws IOException {
-		Runtime rt = Runtime.getRuntime();
-		// this code for record the screen of your device
-		rt.exec("cmd.exe /C adb shell screenrecord /sdcard/runCase.mp4");
+	// @BeforeClass(alwaysRun = true)
+	// public void startRecord() throws IOException {
+	//
+	// Process p = Runtime
+	// .getRuntime()
+	// .exec("cmd.exe /C \"C:\\Program Files (x86)\\Appium\\node_modules\\.bin\\appium.cmd\"");
+	// final InputStream is1 = p.getInputStream();
+	// new Thread(new Runnable() {
+	// public void run() {
+	// BufferedReader br = new BufferedReader(new InputStreamReader(
+	// is1));
+	// try {
+	// while (br.readLine() != null)
+	// ;
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// }).start(); // 启动单独的线程来清空p.getInputStream()的缓冲区
+	// InputStream is2 = p.getErrorStream();
+	// BufferedReader br2 = new BufferedReader(new InputStreamReader(is2));
+	// StringBuilder buf = new StringBuilder(); // 保存输出结果流
+	// String line = null;
+	// while ((line = br2.readLine()) != null)
+	// buf.append(line); //
+	// System.out.println("输出结果为：" + buf);
+	// // Runtime rt = Runtime.getRuntime();
+	// // // this code for record the screen of your device
+	// //
+	// rt.exec("cmd.exe /C \"C:\\Program Files (x86)\\Appium\\node_modules\\.bin\\appium.cmd\"");
+	// // rt.exec("cmd.exe /C adb shell screenrecord /sdcard/runCase.mp4");
+	//
+	// }
 
-	}
-
-	@BeforeClass(alwaysRun = true)
+	@BeforeMethod(alwaysRun = true)
 	public void setUp() throws Exception {
 		// set up appium
 
@@ -43,14 +73,13 @@ public class slideToUnlock {
 		capabilities.setCapability("deviceName", "Android Emulator");
 		capabilities.setCapability("platformVersion", "5.1");
 		// if no need install don't add this
-		capabilities.setCapability("appPackage",
-				"mobi.w3studio.apps.android.shsmy.phone");
+		capabilities.setCapability("appPackage", "cmb.pb");
 		// no need sign
 		capabilities.setCapability("noSign", "True");
-		capabilities.setCapability("appActivity", ".ui.WelcomeActivity");
+		capabilities.setCapability("appActivity", ".ui.PBInitActivity");
 		driver = new AndroidDriver<WebElement>(new URL(
 				"http://127.0.0.1:4723/wd/hub"), capabilities);
-		startRecord();
+
 	}
 
 	@Test(groups = "swipeTest", priority = 1)
@@ -58,15 +87,61 @@ public class slideToUnlock {
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		// swipe to right
-//		this.swipeToRight(driver, 2500);
-		int width = driver.manage().window().getSize().width;
-		int height = driver.manage().window().getSize().height;
-		final TouchAction touchAction = new TouchAction(driver);
-		// appium converts press-wait-move to-release to a swipe action
-		touchAction.press(width / 4, height / 2).waitAction(2500)
-				.moveTo(width * 3 / 4, height / 2).release().perform();
+		System.out.println(driver.getPageSource());
+		// swipeToRight(driver, 2000);
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElement(By.id("cmb.pb:id/item_funcIcon")).click();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
+		List<WebElement> pic = driver
+				.findElements(By
+						.xpath("//android.widget.FrameLayout/android.widget.ImageView"));
+		for (int i = 0; i < pic.size(); i++) {
+			System.out.println(pic.size());
+			pic.get(i).click();
+		}
+
+		final TouchAction touchAction = new TouchAction(driver);
+		touchAction
+				.press(pic.get(0).getLocation().getX(),
+						pic.get(0).getLocation().getY())
+				.waitAction(1500)
+				.moveTo(pic.get(1).getLocation().getX()
+						- pic.get(0).getLocation().getX(),
+						pic.get(1).getLocation().getY()
+								- pic.get(0).getLocation().getY())
+				.moveTo(pic.get(2).getLocation().getX()
+						- pic.get(1).getLocation().getX(),
+						pic.get(2).getLocation().getY()
+								- pic.get(1).getLocation().getY())
+				.moveTo(pic.get(4).getLocation().getX()
+						- pic.get(2).getLocation().getX(),
+						pic.get(4).getLocation().getY()
+								- pic.get(2).getLocation().getY())
+				.moveTo(pic.get(6).getLocation().getX()
+						- pic.get(4).getLocation().getX(),
+						pic.get(6).getLocation().getY()
+								- pic.get(4).getLocation().getY())
+				.moveTo(pic.get(7).getLocation().getX()
+						- pic.get(6).getLocation().getX(),
+						pic.get(7).getLocation().getY()
+								- pic.get(6).getLocation().getY())
+				.moveTo(pic.get(8).getLocation().getX()
+						- pic.get(7).getLocation().getX(),
+						pic.get(8).getLocation().getY()
+								- pic.get(7).getLocation().getY()).release();
+		touchAction.perform();
+		/*
+		 * final TouchAction touchAction = new TouchAction(driver);
+		 * touchAction.press(pic.get(0)).waitAction(1500).moveTo(pic.get(1))
+		 * .moveTo(pic.get(2)).moveTo(pic.get(4)).moveTo(pic.get(6))
+		 * .moveTo(pic.get(7)).moveTo(pic.get(8)).release();
+		 * touchAction.perform();
+		 */
+		String username = driver.findElement(By.id("cmb.pb:id/gTvMenuTitle"))
+				.getText();
+		System.out.println(username);
 	}
 
 	@AfterClass(alwaysRun = true)
@@ -104,10 +179,15 @@ public class slideToUnlock {
 		}
 	}
 
-	public void swipeToRight(AndroidDriver<?> driver, int during) {
+	public void swipeToRight(MobileDriver driver, int during) {
 		int width = driver.manage().window().getSize().width;
 		int height = driver.manage().window().getSize().height;
-		driver.swipe(width / 4, height / 2, width * 3 / 4, height / 2, during);
+		driver.swipe(width / 4, height / 2, width * 5 / 6, height / 2, during);
 		// wait for page loading
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void stopAppiumServer() {
+
 	}
 }
