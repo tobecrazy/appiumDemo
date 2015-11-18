e48c06bb2cf297aa1075ec638a4963fb3812d7b7
System Administrator <root@Youngs-Mac.local>
2015-11-18 08:17:47 -0800

    use page object model

---
+++ src/com/dbyl/core/iOSPageDemo.java
@@ -0,0 +1,48 @@
+package com.dbyl.core;
+
+/**
+ * @author Young
+ */
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.pagefactory.iOSFindBy;
+
+public class iOSPageDemo {
+	IOSDriver<?> driver;
+
+	@iOSFindBy(name = "TextField1")
+	private WebElement inputbox1;
+	@iOSFindBy(name = "TextField2")
+	private WebElement inputbox2;
+
+	@iOSFindBy(xpath = "//UIAApplication[1]/UIAWindow[2]/UIAButton[1]")
+	private WebElement calcButton;
+	@iOSFindBy(xpath = "//UIAApplication[1]/UIAWindow[2]/UIAStaticText[1]")
+	private WebElement result;
+
+	iOSPageDemo(IOSDriver<?> driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	public void typeInputBox(WebElement e, String msg) {
+		e.sendKeys(msg);
+	}
+
+	public void typeTextField1(String msg) {
+		typeInputBox(inputbox1, msg);
+	}
+
+	public void typeTextField2(String msg) {
+		typeInputBox(inputbox2, msg);
+	}
+
+	public void clickCalcButton() {
+		calcButton.click();
+	}
+
+	public String getResult() {
+		return result.getAttribute("value");
+	}
+}
