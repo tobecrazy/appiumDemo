6246829e9d13e9a3772590555f1b799788442a9f
tobecrazy <dbyl@foxmail.com>
2015-12-23 23:22:50 +0800

    add selendroid and add selendroid test case demo

---
+++ run standserver.bat
@@ -0,0 +1,1 @@
+java -jar libs/selendroid-standalone-0.17.0-with-dependencies.jar  -app selendroid-test-app-0.17.0.apk
--- src/com/dbyl/core/Test.java
+++ src/com/dbyl/core/Test.java
@@ -1,24 +1,31 @@
 package com.dbyl.core;
 
-import java.io.IOException;
 import java.io.InputStream;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import io.selendroid.client.SelendroidDriver;
+import io.selendroid.common.SelendroidCapabilities;
+import io.selendroid.common.device.DeviceTargetPlatform;
+ 
+
 
 public class Test {
 
-	public static void main(String [] args)
+	public static void main(String [] args) throws Exception
 	{
-		Runtime rt=Runtime.getRuntime();
-		
-		   try {     
-		        Process process = rt.exec("cmd.exe /C appium");   
-		        InputStream in = process.getInputStream();     
-		        while (in.read() != -1) {   
-		            System.out.print(in.read());   
-		        }   
-		        in.close();   
-		       // process.waitFor();   
-		    } catch (Exception e) {            
-		        e.printStackTrace();   
-		    }   
+		SelendroidCapabilities capa = new SelendroidCapabilities("io.selendroid.testapp:0.17.0");
+//		capa.setPlatformVersion(DeviceTargetPlatform.ANDROID22);
+//		capa.setEmulator(false);
+//		capa.setModel("MeiZu");
+		WebDriver driver = new SelendroidDriver(capa);
+		WebElement inputField = driver.findElement(By.id("my_text_field"));
+		Assert.assertEquals("true", inputField.getAttribute("enabled"));
+		inputField.sendKeys("Selendroid");
+		Assert.assertEquals("Selendroid", inputField.getText());
+		driver.quit();
 	}
 }
