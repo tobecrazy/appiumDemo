1cf47e0d25cacaa2d3b81954bef1d790a783b692
tobecrazy <dbyl@foxmail.com>
2016-01-07 01:00:30 +0800

    modify and fix the old test scripts

--- src/main/java/com/dbyl/appiumCore/JDTest.java
+++ src/main/java/com/dbyl/appiumCore/JDTest.java
@@ -8,18 +8,36 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
  
 public class JDTest {
-	private  AndroidDriver  driver;
+	private  AndroidDriver<WebElement>  driver;
  
+	@BeforeClass(alwaysRun=true)
+	public void startAppiumServer() throws IOException
+	{
+//		 Process p = Runtime
+//		 .getRuntime()
+//		 .exec("cmd.exe /C appium");
+//		 BufferedReader bf = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+//		 String line = null;
+//		 while((line = bf.readLine()) != null) {
+//		 System.out.println(line);
+//		 }
+		
+	}
     @BeforeMethod(alwaysRun=true)
     public void setUp() throws Exception {
         // set up appium
@@ -30,7 +48,7 @@ public class JDTest {
         capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName","Android Emulator");
-        capabilities.setCapability("platformVersion", "4.4");
+        capabilities.setCapability("platformVersion", "5.1");
         //if no need install don't add this
         //capabilities.setCapability("app", app.getAbsolutePath());
         capabilities.setCapability("appPackage", "com.jingdong.app.mall");
@@ -40,23 +58,28 @@ public class JDTest {
         //no need sign
         capabilities.setCapability("noSign", "True");
         capabilities.setCapability("appActivity", ".MainActivity");
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
  
-    @AfterMethod(alwaysRun=true)
-    public void tearDown() throws Exception {
-        driver.quit();
-    }
+
  
     @Test(groups={"JDTest"})
     public void addContact(){
-        WebElement el = driver.findElement(By.name("我的"));
+        WebElement el = driver.findElementByXPath("//android.widget.FrameLayout//android.widget.RadioButton[4]");
         el.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+        WebElement loginButton=driver.findElementById("com.jingdong.app.mall:id/dit");
+        loginButton.click();
         List<WebElement> textFieldsList = driver.findElementsByClassName("android.widget.EditText");
         textFieldsList.get(0).sendKeys("Some Name");
         textFieldsList.get(1).sendKeys("10086");
         textFieldsList.get(2).sendKeys("Some@example.com");
         driver.swipe(100, 500, 100, 100, 2);
         driver.findElementByName("Save").click();
+    }
+    
+    @AfterMethod(alwaysRun=true)
+    public void tearDown() throws Exception {
+        driver.quit();
     }
 }
