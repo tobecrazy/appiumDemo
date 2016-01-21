57a8caa0eacfe614b5b3f3895446710bca9ecf07
tobecrazy <dbyl@foxmail.com>
2016-01-21 21:34:49 +0800

    add webview test case

--- src/main/java/com/dbyl/appiumCore/tests/WebViewHomeWork.java
+++ src/main/java/com/dbyl/appiumCore/tests/WebViewHomeWork.java
@@ -1,13 +1,93 @@
 package main.java.com.dbyl.appiumCore.tests;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.MobileCapabilityType;
+import main.java.com.dbyl.appiumServer.AppiumServerUtils;
 
 public class WebViewHomeWork
 {
-    public static void main(String[] args)
+    private AndroidDriver<MobileElement> driver;
+    private String                       keyword = "appium";
+
+    @BeforeClass
+    public void startAppiumServer() throws IOException, InterruptedException
     {
-        int a = 10;
-        int b = 12;
-        int c = a + b;
-        System.out.println(c);
+        AppiumServerUtils.startServer("127.0.0.1", 4723);
     }
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception
+    {
+        // set up appium
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.APPIUM);
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.1");
+        // if no need install don't add this
+
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "apps");
+        File app = new File(appDir, "webview.apk");
+        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+
+        capabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "com.testerhome.webview");
+        // // support Chinese
+        // capabilities.setCapability("unicodeKeyboard", "True");
+        // capabilities.setCapability("resetKeyboard", "True");
+        // no need sign
+        capabilities.setCapability("noSign", "True");
+        capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, ".MainActivity");
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+    }
+
+    @Test(groups = { "webView" })
+    public void webViewTest()
+    {
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        Set<String> windows = driver.getContextHandles();
+        for (String window : windows)
+        {
+            System.out.println(window);
+        }
+        driver.context("WEBVIEW_com.testerhome.webview");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.findElementByXPath("//textarea[@id='index-kw']").sendKeys(keyword);
+        driver.tap(1, driver.findElement(By.xpath("//button[@id='index-bn']")), 100);
+        // driver.findElement(By.xpath("//button[@id='index-bn']")).click();
+
+        driver.context("NATIVE_APP");
+        WebElement title = driver.findElementById("android:id/action_bar_title");
+        Assert.assertEquals(title.getText(), keyword);
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception
+    {
+        driver.quit();
+        AppiumServerUtils.stopAppiumServer("4723");
+    }
+
 }
--- src/main/java/com/dbyl/appiumCore/tests/baidu.java
+++ src/main/java/com/dbyl/appiumCore/tests/baidu.java
@@ -1,8 +1,8 @@
 package main.java.com.dbyl.appiumCore.tests;
 
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
@@ -20,85 +20,90 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-public class baidu {
-	AndroidDriver<WebElement> driver;
+public class baidu
+{
+    AndroidDriver<WebElement> driver;
 
-	/**
-	 * @author Young
-	 * @throws IOException
-	 */
-	public void startRecord() throws IOException {
-		Runtime rt = Runtime.getRuntime();
-		// this code for record the screen of your device
-		rt.exec("cmd.exe /C adb shell screenrecord /sdcard/runCase.mp4");
+    /**
+     * @author Young
+     * @throws IOException
+     */
+    public void startRecord() throws IOException
+    {
+        Runtime rt = Runtime.getRuntime();
+        // this code for record the screen of your device
+        rt.exec("cmd.exe /C adb shell screenrecord /sdcard/runCase.mp4");
 
-	}
+    }
 
-	@BeforeClass(alwaysRun = true)
-	public void setUp() throws Exception {
-		// set up appium
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception
+    {
+        // set up appium
 
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
-		capabilities.setCapability("platformName", "Android");
-		capabilities.setCapability("deviceName", "Android Emulator");
-		capabilities.setCapability("platformVersion", "5.1");
-		driver = new AndroidDriver<WebElement>(new URL(
-				"http://127.0.0.1:4723/wd/hub"), capabilities);
-		startRecord();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Android Emulator");
+        capabilities.setCapability("platformVersion", "5.1");
+        driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        startRecord();
 
-	}
+    }
 
-	@Test
-	public void runChromeWebBrowser() {
-		driver.get("http://www.baidu.com");
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		System.out.println(driver.getTitle());
-		snapshot((TakesScreenshot) driver, "before_search.png");
-		driver.findElementByXPath("//textarea[@id='index-kw']").sendKeys(
-				"appium");
-		driver.tap(1,driver.findElement(By.xpath("//button[@id='index-bn']")), 100);
-//		driver.findElement(By.xpath("//button[@id='index-bn']")).click();
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		System.out.println(driver.getTitle());
-		snapshot((TakesScreenshot) driver, "after_search.png");
-		Assert.assertTrue(driver.getTitle().contains("appium"));
+    @Test
+    public void runChromeWebBrowser()
+    {
+        driver.get("http://www.baidu.com");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        System.out.println(driver.getTitle());
+        snapshot((TakesScreenshot) driver, "before_search.png");
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("document.getElementById('index-kw').value='appium'");
+        // driver.findElementByXPath("//textarea[@id='index-kw']").sendKeys("appium");
+        // driver.tap(1,
+        // driver.findElement(By.xpath("//button[@id='index-bn']")), 100);
+        driver.findElement(By.xpath("//button[@id='index-bn']")).click();
+        System.out.println(driver.getTitle());
+        // snapshot((TakesScreenshot) driver, "after_search.png");
+        Assert.assertTrue(driver.getTitle().contains("appium"));
 
-	}
+    }
 
-	/**
-	 * This Method create for take screenshot
-	 * 
-	 * @author Young
-	 * @param drivername
-	 * @param filename
-	 */
-	public static void snapshot(TakesScreenshot drivername, String filename) {
-		// this method will take screen shot ,require two parameters ,one is
-		// driver name, another is file name
+    /**
+     * This Method create for take screenshot
+     * 
+     * @author Young
+     * @param drivername
+     * @param filename
+     */
+    public static void snapshot(TakesScreenshot drivername, String filename)
+    {
+        // this method will take screen shot ,require two parameters ,one is
+        // driver name, another is file name
 
-		String currentPath = System.getProperty("user.dir"); // get current work
-																// folder
-		File scrFile = drivername.getScreenshotAs(OutputType.FILE);
-		// Now you can do whatever you need to do with it, for example copy
-		// somewhere
-		try {
-			System.out.println("save snapshot path is:" + currentPath + "/"
-					+ filename);
-			FileUtils
-					.copyFile(scrFile, new File(currentPath + "\\" + filename));
-		} catch (IOException e) {
-			System.out.println("Can't save screenshot");
-			e.printStackTrace();
-		} finally {
-			System.out.println("screen shot finished, it's in " + currentPath
-					+ " folder");
-		}
-	}
+        String currentPath = System.getProperty("user.dir"); // get current work
+                                                             // folder
+        File scrFile = drivername.getScreenshotAs(OutputType.FILE);
+        // Now you can do whatever you need to do with it, for example copy
+        // somewhere
+        try
+        {
+            System.out.println("save snapshot path is:" + currentPath + "/" + filename);
+            FileUtils.copyFile(scrFile, new File(currentPath + "\\" + filename));
+        } catch (IOException e)
+        {
+            System.out.println("Can't save screenshot");
+            e.printStackTrace();
+        } finally
+        {
+            System.out.println("screen shot finished, it's in " + currentPath + " folder");
+        }
+    }
 
-	@AfterClass
-	public void afterTestStopDriver() {
-		driver.quit();
-	}
+    @AfterClass
+    public void afterTestStopDriver()
+    {
+        driver.quit();
+    }
 }
