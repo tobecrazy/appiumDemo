d4577db5796497c72dae08720c797eb519c0e9ab
tobecrazy <dbyl@foxmail.com>
2016-11-16 00:04:50 +0800

    update webview apk

--- src/main/java/com/dbyl/appiumCore/tests/AppWebView.java
+++ src/main/java/com/dbyl/appiumCore/tests/AppWebView.java
@@ -7,8 +7,14 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -21,17 +27,18 @@ import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
 import main.java.com.dbyl.appiumServer.AppLogger;
+import main.java.com.dbyl.appiumServer.AppiumServerUtils;
 
 public class AppWebView {
-	private AndroidDriver<MobileElement> driver;
-	private String keyword = "appium";
-	AppLogger logger= new AppLogger(AppWebView.class);
+	AndroidDriver<MobileElement> driver;
+	String keyword = "appium";
+	URL url;
+	AppLogger logger = new AppLogger(AppWebView.class);
 
 	@BeforeClass
 	public void startAppiumServer() throws IOException, InterruptedException {
-		// AppiumServerUtils.startServer("127.0.0.1", 4723);
-		Thread.sleep(1000);
-		logger.debug("sleep 1 s");
+		url = AppiumServerUtils.getInstance().startServer("127.0.0.1", 4723);
+		logger.debug("start Appium Server");
 	}
 
 	@BeforeMethod(alwaysRun = true)
@@ -56,41 +63,74 @@ public class AppWebView {
 		// no need sign
 		capabilities.setCapability("noSign", "True");
 		capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".MainActivity");
-		logger.info("connet to Appium server :127.0.0.1:4723");
-		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		
+		logger.info("connet to Appium server  " + url.toString());
+		driver = new AndroidDriver<MobileElement>(url, capabilities);
+
 	}
 
 	@Test(groups = { "webView" })
 	public void webViewTest() throws InterruptedException {
-		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		MobileElement clickToPresent = driver.findElementById("com.testerhome.webview:id/action_present");
+		clickToPresent.click();
+		MobileElement imageButton = driver.findElementById("com.testerhome.webview:id/button");
+		Assert.assertTrue(imageButton.isDisplayed());
 		Set<String> contexts = driver.getContextHandles();
 		for (String context : contexts) {
 			// 输出context
 			logger.info(context);
-			if(context.contains("WEBVIEW"))
-			{
-				logger.info("swich_to_content "+context);
+			if (context.contains("WEBVIEW")) {
+				logger.info("swich_to_content " + context);
 				driver.context(context);
 			}
 		}
-		Thread.sleep(5000);
 		logger.info(driver.getContext());
-	 
-		driver.findElementByXPath("//input[@id='index-kw']").sendKeys(keyword);
-		driver.findElement(By.xpath("//button[@id='index-bn']")).click();
+		// 隐式等待，针对全局设置
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		// pageLoadTimeout只能用于webview 或者 pure web
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		// 这段代码返回浏览器加载页面状态，complete为加载完成
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		String status = (String) jse.executeScript("var status=document.readyState;return status");
+		Assert.assertTrue(status.contains("complete"));
+		WebDriverWait wait = new WebDriverWait(driver, 30);
 
+		WebElement input = wait.until(new ExpectedCondition<WebElement>() {
+			@Override
+			public WebElement apply(WebDriver driver) {
+				return driver.findElement(By.xpath("//input[@id='index-kw']"));
+			}
+		});
+		input.sendKeys(keyword);
+		By by = By.xpath("//button[@id='index-bn']");
+		if (isElementPresent(by)) {
+			driver.findElement(by).click();
+		}
+
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.navigate().refresh();
 		// 返回native app context
 		driver.context("NATIVE_APP");
 		MobileElement title = driver.findElementById("android:id/action_bar_title");
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		Assert.assertTrue(title.getText().contains(keyword));
-		
-		MobileElement back=driver.findElementById("com.testerhome.webview:id/action_back");
+		MobileElement back = driver.findElementById("com.testerhome.webview:id/action_back");
 		back.click();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		Assert.assertTrue(title.getText().equals("百度一下"));
 
+	}
+
+	/**
+	 * @author young
+	 * @param by
+	 * @return
+	 */
+	private boolean isElementPresent(By by) {
+		try {
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
 	}
 
 	@AfterMethod(alwaysRun = true)
